9168514cbca8f6b99d34d4f7f23608c23c4bf531
hevox1 <hevox1231@gmail.com>
2025-12-15 10:58:26 +0300

    architecture

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
-from dotenv import load_dotenv
 import asyncio
-import os
 import logging
+from loader import dp, bot
 
-# Загружаем переменные из .env
-load_dotenv()
-
+# Настройка логирования
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-bot=Bot(BOT_TOKEN)
-dp = Dispatcher()
 
 @dp.message(Command('start'))
 async def start(message: Message):
